812a043a46a1a0f9111f3bb73e8341d2d052f407
Thiago Correia <thiagogithub2002@gmail.com>
2023-01-10 01:15:56 -0300

    outra atividade

--- resources/views/act.php
+++ resources/views/act.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Templates</title>
+    <title>act</title>
 </head>
 <body>
-    <h1>Meu primeiro template Blade</h1>
+    <h2>Instituto de Ciência e Tecnologia de Franca</h2>
 </body>
 </html>
