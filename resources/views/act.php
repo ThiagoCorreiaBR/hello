7faaf61345d7342bf27ceff0c8d628ece73935a0
Thiago Correia <thiagogithub2002@gmail.com>
2023-01-10 19:02:06 -0300

    templates

--- resources/views/act.php
+++ resources/views/act.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template</title>
+    <title>Templates</title>
 </head>
 <body>
     <h2>Instituto de Ciência e Tecnologia de Franca</h2>
